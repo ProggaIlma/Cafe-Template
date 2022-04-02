800bdb0cc3a114f892414d66eaa2ca33fb3710c8
Nagifa <nagifap@techved.com>
2022-04-02 14:06:05 +0600

    nav bar fixed

--- src/app/pages/super-admin/super-admin-routing.module.ts
+++ src/app/pages/super-admin/super-admin-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled'})],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
 export class SuperAdminRoutingModule { }
