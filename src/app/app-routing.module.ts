e165dc3348a00c813db6aa6784267f2f735adcc1
Nagifa <nagifap@techved.com>
2022-05-24 14:22:24 +0600

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BannerComponent } from './components';
-import { LayoutComponent } from './layout/layout.component';
 import {PortfolioComponent} from './pages/portfolio/portfolio.component';
 
 
