bff2a33c12194804e37d573b480345be212bc9cb
Nagifa <nagifap@techved.com>
2022-05-24 20:08:54 +0600

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BannerComponent } from './components';
+// import { LayoutComponent } from './layout/layout.component';
 import {PortfolioComponent} from './pages/portfolio/portfolio.component';
 
 
 const routes: Routes = [
-  
   {
-    path: 'cafe',
+    path: '',
+  component: BannerComponent,
+  pathMatch: 'full'
+  },
+  {
+    path: 'index',
     component: BannerComponent
   },
   {
-    path: 'shop',
+    path: 'portfolio',
   component: PortfolioComponent,
        
   },
   {
     path: 'dashboard',
     loadChildren: () => import('./pages/super-admin/super-admin.module').then((m) => m.SuperAdminModule)
-  }
+  },
+ 
+  {
+    path: '**',
+    redirectTo: 'super-admin',
+  },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
